9d74c5e6f629073efeaead6dcc0fd5b2dc8df0b4
Abhi13jain <abhiijain13@gmail.com>
2025-01-11 15:15:57 +0530

    add db connect

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express"
 import cors from 'cors'
+import { connectDB } from "./config/db.js"
 import 'dotenv/config'
 
 // app config
 const app = express()
 const port = 5000
 
-
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+// db connection
+connectDB()
 
 app.get("/", (req, res) => {
   res.send("working")
